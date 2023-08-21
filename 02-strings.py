f81c707044ed5fe7c8bddbbd1699896f58375e03
Dalb3rg03 <142837411+Dalb3rg03@users.noreply.github.com>
2023-08-21 23:46:40 +0200

    Create 02-strings.py

---
+++ 02-strings.py
@@ -0,0 +1,12 @@
+nombre_curso = "ultimate python"
+descripcion = """
+jfndsijanfjnsdjkfnjsbnfnfjnfjknfjknfjknfkjnsfjnfkjnfjs
+efsefikofmkmkfmrjerrmrfkojmpmfkmkfmkfmfklmfklmfklmkmfkfmklfmklfmklsmklf
+ksmmsiuuuuuuumessi
+"""
+print(len(nombre_curso))#te dice cuantas cifras tiene el texto
+print(nombre_curso[0])#para elejir cual letras de todas elejir ([])
+print(nombre_curso[3:5])#a la izquierda desde donde inicia y a la derecha de donde termina ([:])
+print(nombre_curso[3:])#si no rellenas la casilla de la izquierda va a iniciar del 0 al numero que pusiste en la derecha
+#y si no rellenas la casilla de la derecha va a iniciar de la ultima letra hasta ell numero que pusiste en la izquierda y si  no pones ningun nuero ni en la derecha ni la izquierda
+#va a imprimir todo
