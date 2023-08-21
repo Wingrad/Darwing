e71d669aaf44041fbc4993cd21ae94b3b5fa7fc5
Wingrad <142837232+Wingrad@users.noreply.github.com>
2023-08-21 16:39:31 -0400

    Add files via upload

---
+++ borron.py
@@ -0,0 +1,20 @@
+nombre = "Dayeling "
+apellido = "Roa"
+nombre += "Maria "
+print(nombre + apellido)
+
+x = "2"
+x += "34"
+print(x)
+
+videojuego = "Elden Ring"
+videojuego1 = "Persona 5"
+videojuego2 = "Gta" 
+Discurso = f"Yo estoy seguro de que {videojuego}, {videojuego1} y {videojuego2} seran los mejores de este ano"
+print(Discurso)
+
+del videojuego1
+print(Discurso)
+
+print("sona" in Discurso)
+print("Elden" not in Discurso)
