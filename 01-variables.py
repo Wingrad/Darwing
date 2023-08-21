4b0cb8f3fb4a28adf6e8d7814e3315462bce86b2
Dalb3rg03 <142837411+Dalb3rg03@users.noreply.github.com>
2023-08-21 23:45:04 +0200

    Create 01-variables.py
    aqui para que te acuerdes del float, interger y stings

---
+++ 01-variables.py
@@ -0,0 +1,8 @@
+nombre_curso = "ultimate python"
+nombre_1 = "HOLA"
+Nombre_curso = "QUE"
+NOMBRE_1 = "KLK"
+print(nombre_curso, nombre_1, Nombre_curso, NOMBRE_1)
+alumnos = 5000#NUMERO ENTERO (INTERGER)
+puntaje = 9.99999999#NUMERO DECIMAL (FLOAT)
+publicado = True#texto(string)
