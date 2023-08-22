175b1138d14704b6b160a9df1acd58d8b331c37d
Dalb3rg03 <142837411+Dalb3rg03@users.noreply.github.com>
2023-08-22 13:59:47 -0400

    Add files via upload

---
+++ 02-02-if.py
@@ -0,0 +1,10 @@
+edad = 90
+if edad >= 60:
+    print("usted entra gratis")
+elif edad >= 50:
+    print("tienes un descuento")
+elif edad > 17:
+    print("puedes ver la pelicula")
+else:
+    print("no puedes entrar pequeño saltamontes")
+    print("te toca verla en el cuevana")
